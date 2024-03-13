ad44b5e39065cd0c7434111bd27a03684872b2d8
Dima2430 <dima.volkkom@gmail.com>
2024-03-13 17:18:32 +0100

    update

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,29 +1,31 @@
-import ContactForm from './Components/ContactForm/ContactForm';
-import ContactList from './Components/ContactList/ContactList';
-import { useState,useEffect } from 'react';
-import SearchBox from './Components/SearchBox/SearchBox';
-import { nanoid } from 'nanoid';
+import ContactForm from "./ContactForm/ContactForm";
+import ContactList from "./ContactList/ContactList";
+import { useState, useEffect } from "react";
+import SearchBox from "./SearchBox/SearchBox";
+import { nanoid } from "nanoid";
 
- const initialContacts = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+const initialContacts = [
+  { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+  { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
 ];
 function App() {
-   const [contacts, setContacts] = useState(() => {
-    const savedContacts = localStorage.getItem('contacts');
-    return savedContacts && savedContacts !== 'undefined' ? JSON.parse(savedContacts) : initialContacts;
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = localStorage.getItem("contacts");
+    return savedContacts && savedContacts !== "undefined"
+      ? JSON.parse(savedContacts)
+      : initialContacts;
   });
-  const [filter, setFilter] = useState('');
+  const [filter, setFilter] = useState("");
 
   useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
+    localStorage.setItem("contacts", JSON.stringify(contacts));
   }, [contacts]);
 
   const addContact = (newContact) => {
-     const contactWithId = { ...newContact, id: nanoid() };
-  setContacts([...contacts, contactWithId]);
+    const contactWithId = { ...newContact, id: nanoid() };
+    setContacts([...contacts, contactWithId]);
   };
 
   const deleteContact = (contactId) => {
